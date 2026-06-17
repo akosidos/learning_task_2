d7698c37041bb6c4edbd4b63e95511069aae8740
akosidos <rueljrexanes13@gmail.com>
2026-06-17 13:11:24 +0800

    Add files via upload

---
+++ bsit_1b/activity_05/activity_05.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>..: Activity 05 :..</title>
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>
+    <body>
+        <div class= "wrapper">
+            <div class="header">
+            </div>  
+            <div class="container">
+            </div>
+            <div class="footer"> </div>
+        </div>
+    </body>
+</html>
